550cb080e5a14ccdd89a0cdd79eefad46456d7a9
ofay11 <itimurdautov@gmail.com>
2023-08-25 21:35:08 +0300

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,23 @@
 #!/usr/bin/env node
 
-const { Command } = require('commander');
+import { Command } from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
 program
-  .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number')
-  .option('-h, --help', 'output usage information')
-  .parse(process.argv);
+  .description('Compares two JSON files and shows a difference.')
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .action((filePath1, filePath2) => {
+    const diffOutput = genDiff(filePath1, filePath2, program.opts().format);
+    console.log(diffOutput);
+  });
 
-if (program.help) {
-  console.log(program.helpInformation());
+program.parse();
+
+if (program.args.length !== 2) {
+  console.error('Please provide two filepaths for comparison.');
+  program.help();
 }
