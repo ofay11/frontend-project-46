315bfd391a78cf27792aadb4efbc9bd9718df9bf
ofay11 <itimurdautov@gmail.com>
2023-09-23 18:38:27 +0300

    fix for catching errors in the number of arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,18 +6,17 @@ import genDiff from '../src/index.js';
 const program = new Command();
 
 program
-  .description('Compares two JSON files and shows a difference.')
+  .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format')
   .action((filePath1, filePath2) => {
+    if (program.args.length !== 2) {
+      console.error('\nPlease provide two filepaths for comparison.');
+      program.help();
+    }
     const diffOutput = genDiff(filePath1, filePath2, program.opts().format);
     console.log(diffOutput);
   });
 
 program.parse();
-
-if (program.args.length !== 2) {
-  console.error('Please provide two filepaths for comparison.');
-  program.help();
-}
