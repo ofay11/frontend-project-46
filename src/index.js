15a499c611a0c3934d05acbf084e1590f5d761a3
ofay11 <itimurdautov@gmail.com>
2023-09-23 18:47:39 +0300

    upd gendiff index

--- src/index.js
+++ src/index.js
@@ -1,53 +1,26 @@
 import fs from 'fs';
 import path from 'path';
-import _ from 'lodash';
-import parseDataByFormat from './parsers.js';
+import buildTree from './buildTree.js';
+import parse from './parsers.js';
+import formatOutput from './formatters/index.js';
 
-const getAbsolutePath = (filepath) => {
-  const normalizedPath = path.normalize(filepath);
-  const absolutePath = path.isAbsolute(normalizedPath)
-    ? normalizedPath
-    : path.resolve(process.cwd(), normalizedPath);
-  return absolutePath;
+const getAbsolutePath = (file) => path.resolve(process.cwd(), file);
+const getFileExtensions = (file) => path.extname(file).slice(1);
+
+const getContentFromFile = (file) => {
+  const absolutePath = getAbsolutePath(file);
+  const data = fs.readFileSync(absolutePath, 'utf8');
+  const extension = getFileExtensions(file);
+  return parse(data, extension);
 };
 
-const readFile = (filepath) => {
-  const absolutePath = getAbsolutePath(filepath);
-  try {
-    return fs.readFileSync(absolutePath, 'utf-8');
-  } catch (error) {
-    throw new Error(`Unable to read file at path: ${absolutePath}`);
-  }
+const gendiff = (filePath1, filePath2, format = 'stylish') => {
+  const file1 = getContentFromFile(filePath1);
+  const file2 = getContentFromFile(filePath2);
+  const diffInfo = buildTree(file1, file2);
+  const formattedTree = formatOutput(diffInfo, format);
+
+  return formattedTree;
 };
 
-const getFileExtension = (filepath) => path.extname(filepath);
-
-const getFileData = (filePath) => {
-  const fileData = readFile(filePath);
-  const fileExtension = getFileExtension(filePath);
-  return parseDataByFormat(fileData, fileExtension);
-}
-
-const genDiff = (data1, data2) => {
-  const fileData1 = getFileData(data1);
-  const fileData2 = getFileData(data2);
-
-  const keys = _.union(Object.keys(fileData1), Object.keys(fileData2)).sort();
-  const diff = keys.map((key) => {
-    if (!_.has(fileData2, key)) {
-      return `- ${key}: ${fileData1[key]}`;
-    }
-    if (!_.has(fileData1, key)) {
-      return `+ ${key}: ${fileData2[key]}`;
-    }
-    if (fileData1[key] === fileData2[key]) {
-      return `  ${key}: ${fileData1[key]}`;
-    }
-    return `- ${key}: ${fileData1[key]}\n  + ${key}: ${fileData2[key]}`;
-  });
-
-  const diffOutput = `{\n  ${diff.join('\n  ')}\n}`;
-  return diffOutput;
-};
-
-export default genDiff;
+export default gendiff;
