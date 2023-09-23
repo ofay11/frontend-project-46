350fdcbb7070a8ee5b3784ca1237ba7ad70e6c3c
ofay11 <itimurdautov@gmail.com>
2023-09-23 18:49:54 +0300

    add formatters index

---
+++ src/formatters/index.js
@@ -0,0 +1,18 @@
+import makePlain from './plain.js';
+import makeStylish from './stylish.js';
+import makeJson from './json.js';
+
+export default (data, format, replacer) => {
+  switch (format) {
+    case 'stylish':
+      return makeStylish(data, replacer);
+    case 'plain':
+      return makePlain(data);
+    case 'json':
+      return makeJson(data, null, replacer);
+    default:
+      throw new Error(
+        `Invalid file format type: '.${format}'! Try supported file formats: 'stylish', 'plain', 'json'.\n`
+      );
+  }
+};
